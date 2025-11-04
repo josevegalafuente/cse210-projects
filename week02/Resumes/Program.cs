8ceb3647ea62aa6bf693d9dce928eddcf0bf96a3
josevegalafuente <jvegalafuente@byupathway.edu>
2025-11-04 15:36:39 -0400

    Finished Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Vendor Manager";
+        job1._company = "Home River Group";
+        job1._startYear = 2019;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Virtual Assistant";
+        job2._company = "Spark6";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jose Vega";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
